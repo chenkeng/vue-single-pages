483a98a72bdd37c8c8c81b4f12231d363b50f901
chenkeng <chenchaohan@outlook.com>
2018-01-17 02:25:17 +0800

    add a es6 project

---
+++ es6/tasks/util/args.js
@@ -0,0 +1,29 @@
+import yargs from 'yargs';
+const args = yargs
+    .option('production',{
+        boolean:true,
+        default:false,
+        describe:'min all scripts'
+    })
+    .option('watch',{
+        boolean:true,
+        default:false,
+        describe:'watch all files'
+    })
+    .option('verbose',{
+        boolean:true,
+        default:false,
+        describe:'log'
+    })
+    .option('sourcemaps',{
+        describe:'force the creation of sourcemaps'
+    })
+    .option('port',{
+        string:true,
+        default:true,
+        describe:'server port'
+    })
+    .argv
+
+
+// verbose  日志
