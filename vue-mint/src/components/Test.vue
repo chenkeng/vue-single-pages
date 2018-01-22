460845c44c46f426fa5cf383c6def3e9c12ed258
chenkeng <chenchaohan@outlook.com>
2018-01-22 14:27:26 +0800

    add picker component

---
+++ vue-mint/src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     <p>{{msg}}</p> 
+  </div>
+</template>
+<style scoped>
+    p{
+        line-height: 1;
+        height: 50px;
+        font-size: 30px;
+        background-color: brown;
+    }
+</style>
+<script>
+export default {
+  name:'Test',
+  data(){
+      return {
+          msg :"superhan is a great programer"
+      }
+  }
+}
+</script>
--- vue-mint/src/router/index.js
+++ vue-mint/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Picker from '@/components/Picker'
+import Test from '@/components/Test'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/picker',
+      name:'Picker',
+      component:Picker
+    },
+    {
+      path:'/test',
+      name:'Test',
+      component:Test
     }
   ]
 })
